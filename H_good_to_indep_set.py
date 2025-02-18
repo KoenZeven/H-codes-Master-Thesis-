0519c0a80a18ae9b419a475b4527b1e20dbdb720
KoenZeven <123657376+KoenZeven@users.noreply.github.com>
2025-02-18 13:27:04 +0100

    Koen: copy local py file to repository

---
+++ H_good_to_indep_set.py
@@ -0,0 +1,116 @@
+import gurobipy as gp
+from gurobipy import GRB
+import networkx as nx
+import time
+import itertools
+
+def IndependentSet(size:int, M:list):
+    """ The ILP to solve the independent set problem, given an adjacency matrix M and number of vertices given by 'size'.
+    
+    Input
+        :int size: number of vertices of the graph
+        :list M: adjacency matrix for the graph
+    
+    Output:
+        :list x: optimal solution to the independent set problem, gives 1's where the vertex is in the independent set"""
+    starttime = time.time()
+
+    # Create a new model
+    m1 = gp.Model("newModel")
+    m1.Params.LogToConsole = 0
+
+    # Add variables
+    x = [m1.addVar(vtype=GRB.INTEGER, name="x["+ str(s)+"]") for s in range(size)]
+
+    # Set objective function
+    m1.setObjective(sum(x[i] for i in range(size)), GRB.MAXIMIZE)
+
+    # Add simple constraints
+    for i in range(size):
+        for j in range(i):
+            if M[i][j] == 1:
+                m1.addConstr(x[i] + x[j] <= 1)
+        m1.addConstr(x[i] <= 1)
+
+    # Optimize
+    m1.optimize()
+
+    
+    endtime = time.time()
+    return m1.ObjVal, endtime - starttime 
+
+def BuildAdjancencyMatrix(graphs:list[nx.Graph], H: list[nx.Graph]) -> tuple[int,list]:
+    """Builds the adjacency matrix of the following graph: 
+    Vertices are the graphs in the list 'graphs', edges are between two graphs whose symmetric difference is in H
+    
+    Input
+    :list[nx.Graph] graphs: list of graphs to be studied
+    :list[nx.Graph] H: family of graphs not to be seen in symmetric difference"""
+    size = len(graphs)
+    M = [[0 for _ in range(i)] for i in range(size)]
+
+    for i in range(size):
+        for j in range(i):
+            if symmetric_diff_contains_an_h(graphs[i], graphs[j], H):
+                M[i][j] = 1
+    return size, M
+
+def symmetric_difference(g1:str, g2:str):
+    x = str()
+    for i in range(len(g1)):
+        if g1[i] != g2[i]:
+            x += '1'
+        else:
+            x += '0'
+    return x
+
+def find_edge_bit_location(number_of_nodes:int, node1:int, node2:int) -> int:
+    """In binary edge representation, returns the bit belonging to the edge (node1,node2), based on the number of nodes"""
+    return (node1 - 1) * number_of_nodes - node1 * (node1 - 1) // 2 + node2 - node1
+
+def symmetric_diff_contains_an_h(g1:str, g2:str, H:list) -> bool:
+    """Computes whether the symmetric difference of the input graphs has a subgraph which is isomorphic to a graph in the list H. 
+    This function is currently unneccesary.
+    
+    Input: 
+        :str g1, g2: binary input graphs to be analysed
+        :list H: family of graphs
+        
+    Output:
+        :bool 1/0: 1 if True, 0 if not"""
+    
+    symm_diff = symmetric_difference(g1, g2)
+    print(g1, g2, symm_diff)
+    for H_graph in H:
+        for subgraph in symm_diff:
+            # Not correct yet
+            # This would need all subgraphs of symm_diff of size equal to that of the H_graph
+            # You would have to check (n choose |H_graph|) possibilities, which is polynomial but sloooow
+            if 0:
+                return 1
+    return 0
+
+def BuildGraphs(n: int) -> list[str]:
+    """Generates all graphs on n vertices and returns their binary string representations."""
+    graphs = []
+    num_edges = n * (n - 1) // 2  # Number of edges in the complete graph (excluding loops)
+    
+    # Iterate over all possible edge combinations
+    for edges in itertools.product("01", repeat=num_edges):
+        binary_string = "".join(edges)  # Convert tuple to string
+        graphs.append(binary_string)
+
+    return graphs
+
+def main():
+    n = 4
+    graphs_n = BuildGraphs(n)
+    print(graphs_n)
+    size, M = BuildAdjancencyMatrix(graphs_n, [nx.complete_graph(3)])
+    print(size,M)
+    optimal, timing = IndependentSet(size,M)
+    print(optimal, timing)
+    return
+
+if __name__ == "__main__":
+    main()
