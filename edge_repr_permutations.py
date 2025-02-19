dc66d37bb6f109226d208c86074f84a29a8f6b9d
KoenZeven <123657376+KoenZeven@users.noreply.github.com>
2025-02-19 16:54:50 +0100

    Koen: start of subgraph isomorphism algorithm

--- edge_repr_permutations.py
+++ edge_repr_permutations.py
@@ -1,6 +1,7 @@
-""" This file is meant to store the functions meant for finding a subgraph in a graph, by chosing the right permutation"""
+""" This file is meant to store the functions meant for finding a subgraph in a graph. Two are given"""
 
 from math import sqrt
+from copy import deepcopy
 
 def edge_to_bit_location(number_of_nodes:int, node1:int, node2:int) -> int:
     """In binary edge representation, returns the bit belonging to the edge (node1,node2), based on the number of nodes.
@@ -30,3 +31,24 @@ def bit_location_to_edge(number_of_nodes:int, bit_location:int) -> tuple[int,int
     node1 = num // 2
     node2 = bit_location - (node1 - 1) * number_of_nodes + node1 * (node1 - 1) // 2 + node1
     return (node1, node2)
+
+
+def find_clique(edge_repr: str, size_clique) -> bool:    
+    """This function computes whether there exists a clique of the input size in the graph, given by its binary representation."""
+    return
+
+def subgraph_isomorphism(step:int, g:str, h_graph:str, h_graphs_number_nodes:int, map_dict:dict, has_been_mapped:list) -> bool:
+    def check_subgraph(mapping_function:dict, g1:str, g2:str):
+        return
+    if step == h_graphs_number_nodes:
+        if check_subgraph(map_dict, g, h_graph):
+            return True
+    for vertex in g: #Incorrect
+        if vertex in has_been_mapped:
+            continue
+        map_dict_copy = deepcopy(map_dict)
+        has_been_mapped_copy = deepcopy(has_been_mapped)
+        map_dict_copy[step] = vertex
+        has_been_mapped_copy.append(vertex)
+        subgraph_isomorphism(step+1, g, h_graph, h_graphs_number_nodes, map_dict_copy, has_been_mapped_copy)
+    return False
